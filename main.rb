5e3316fb4c54d52fe1ed55986596da52d95a0fa7
Andrew Strovers <ADStrovers@gmail.com>
2015-02-11 09:56:19 -0600

    Commit before testing before_filters for loading erb model files.

--- main.rb
+++ main.rb
@@ -6,3 +6,10 @@ require 'active_support/inflector'
 Dir["/models/*.rb"].each {|file| require file }
 
 DATABASE = SQLite3::Database.new("inventory_management.db")
+
+before do
+  erb :homepage
+end
+
+get "/" do
+end
