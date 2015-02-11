495a514240f29e922f207cadfcbbcb855ae67e35
Andrew Strovers <ADStrovers@gmail.com>
2015-02-11 11:04:10 -0600

    Working instance of /create that takes in an array from the Class requested in params and sets labels for form fields.

--- main.rb
+++ main.rb
@@ -3,13 +3,57 @@ require 'sqlite3'
 require 'rubygems'
 require 'active_support/inflector'
 
-Dir["/models/*.rb"].each {|file| require file }
-
 DATABASE = SQLite3::Database.new("inventory_management.db")
 
-before do
-  erb :homepage
+require_relative "models/database_setup"
+require_relative "models/database_functions"
+require_relative "models/category"
+require_relative "models/product"
+require_relative "models/shelf"
+
+before "/" do
+  query_string = params.map{ |key, value| "#{key}=#{value}"}.join("&")
+  case params[:action]
+  when "create"
+    redirect to("/create?#{query_string}")
+  when "view"
+    
+  when "modify"
+    
+  when "delete"
+    
+  end
 end
 
 get "/" do
+  erb :homepage
+end
+
+get "/results" do
+  # Get param with category/product/shelf and redirect the erb call based on that information.
+  # Should allow me to display results for each type in this one spot.
+end
+
+get "/modify" do
+  
+end
+
+get "/delete" do
+  
+end
+
+get "/view" do
+  erb :view
+end
+
+get "/create" do
+  case params[:type]
+  when "shelf"
+    @req = Shelf.requirements
+  when "product"
+    @req = Product.requirements
+  when "category"
+    @req = Category.requirements
+  end
+  erb :create
 end
